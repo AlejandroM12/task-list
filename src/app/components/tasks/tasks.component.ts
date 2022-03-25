b93655fac1df9c6422d51b29b8e4420d4c466134
axel alejandro morel <alejandro.morel1905@gmail.com>
2022-03-25 01:29:10 -0300

    agregando rutas y modificando servicios, componentes

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { TaskService } from '../../service/task.service';
 import { Task } from '../../Task';
 @Component({
@@ -19,5 +20,19 @@ export class TasksComponent implements OnInit {
       this.tasks = tasks
     });
   }
-
+  deleteTask(task:Task){
+    this.taskService.deleteTask(task).subscribe(()=>{
+      this.tasks = this.tasks.filter( (t) => t.id !== task.id)
+    })
+  }
+  toggleReminder(task: Task){
+    task.reminder = !task.reminder;
+    this.taskService.updateTaskReminder(task).subscribe();
+  }
+  addTask(task: Task){
+    this.taskService.addTask(task).subscribe((task) => {
+    this.tasks.push(task);
+    })
+    
+  }
 }
